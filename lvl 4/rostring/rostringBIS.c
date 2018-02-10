ae79118716b2a3018c2c5c60e2460f6e56ef1aa7
Ba Ca <baca@MacBook-Pro-de-Ba.local>
2018-02-10 17:50:43 +0100

    j

---
+++ lvl 4/rostring/rostringBIS.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_strlen(char *str)
+{
+	int i;
+	
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	rostring(char *str)
+{
+	int	i;
+	int	x;
+	char	*dst;
+	char	*tmp;
+
+	i = 0;
+	x = 0;
+	dst = malloc(sizeof(char) * ft_strlen(str) + 1);
+	tmp = malloc(sizeof(char) * ft_strlen(str) + 1);
+	str[ft_strlen(str) + 1] = '\0';
+	while (str[i])
+	{
+		if ((str[i] != ' ' && str[i] != '\t') && str[i])
+		{
+			while ((str[i] != ' ' && str[i] != '\t') && str[i])
+				tmp[x++] = str[i++];
+			break ;
+		}
+		i++;
+	}
+	tmp[x] = '\0';
+	x = 0;
+	i++;
+	while (str[i])
+	{
+		if ((str[i] != ' ' && str[i] != '\t') && str[i] != '\0')
+		{
+			while ((str[i] != ' ' && str[i] != '\t') && str[i] != '\0')
+			{
+				dst[x] = str[i];
+				x++;
+				i++;
+			}
+			dst[x] = ' ';
+			x++;
+		}
+		i++;
+	}
+	dst[x] = '\0';
+	i = 0;
+	while (tmp[i])
+		dst[x++] = tmp[i++];
+	dst[x] = '\0';
+	x = 0;
+	while (dst[x])
+		ft_putchar(dst[x++]);
+	free(tmp);
+	free(dst);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac < 2)
+		ft_putchar('\n');
+	else
+	{
+		rostring(av[1]);
+		ft_putchar('\n');
+	}
+	return (0);
+}			
